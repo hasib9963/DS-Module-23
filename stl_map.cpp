7c21c00d1dcb18606211de282e98f05892b51e5b
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2024-07-11 01:48:12 +0600

    stl map for storing and querying key-value pairs. Inserts and updates values for keys

---
+++ stl_map.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<string, int> mp;
+
+    // Inserting elements into the map
+    mp["Sakib al hasn"] = 79; // Inserts "Sakib al hasn" with value 79
+    mp.insert({"akib", 100}); // Inserts "akib" with value 100
+    mp.insert({"akib", 150}); // Attempts to insert "akib" again with value 150 (updates existing value)
+    mp["tamim"] = 79;         // Inserts "tamim" with value 79
+
+    // Printing all elements in the map
+    for (auto it = mp.begin(); it != mp.end(); it++)
+    {
+        cout << it->first << " " << it->second << endl; // Outputs each key-value pair in the map
+    }
+    return 0;
+}
